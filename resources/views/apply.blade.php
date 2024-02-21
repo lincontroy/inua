cd55c55c344d40dbca40a4090a0b0a72f162dbd7
lincontroy <lincolnmunene37@gmail.com>
2024-02-21 18:27:13 +0300

    Added placeholder

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -14,12 +14,12 @@
                     <label for="loanType">Loan Type</label>
                     <select class="form-control" id="loanType" name="loanType">
                         <option value="">====Select Loan type====</option>
-                        <option value="Emergency mkopo">Emergency mkopo</option>
-                        <option value="Car mkopo">Car mkopo</option>
-                        <option value="Education mkopo">Education mkopo</option>
-                        <option value="Rental mkopo">Rental mkopo</option>
-                        <option value="Business mkopo">Business mkopo</option>
-                        <option value="Personal loan">Personal loan</option>
+                        <option value="Emergency mkopo">Emergency mkopo KES 2,500 (max)</option>
+                        <option value="Car mkopo">Car mkopo KES 500,000 (max)</option>
+                        <option value="Education mkopo">Education mkopo KES 25,000(max)</option>
+                        <option value="Rental mkopo">Rental mkopo KES 20,000(max)</option>
+                        <option value="Business mkopo">Business mkopo KES 50,000(max)</option>
+                        <option value="Personal loan">Personal loan KES 10,000(max)</option>
                     </select>
                 </div>
                 <div class="form-group">
